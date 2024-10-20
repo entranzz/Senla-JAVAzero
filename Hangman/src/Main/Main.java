eb4a5061c7cb9ff23cf74b2d9dbf9e5a0f89de80
Leon Konopko <arcadiimolodec@gmail.com>
2024-10-20 17:25:15 +0300

    updated file strucrute

---
+++ Hangman/src/Main/Main.java
@@ -0,0 +1,11 @@
+package Main;
+
+
+import hangman.HangmanGame;
+
+public class Main {
+    public static void main(String[] args) {
+        HangmanGame game = new HangmanGame();
+        game.play();
+    }
+}
